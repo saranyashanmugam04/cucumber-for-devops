b7d517e437db2c63ce60876c4d7cd9543d073b2f
saranyashanmugam04 <37809422+saranyashanmugam04@users.noreply.github.com>
2019-07-25 15:30:27 +0530

    adding cucumber files

---
+++ src/test/java/pack1/S1Class.java
@@ -0,0 +1,46 @@
+package pack1;
+
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class S1Class {
+	WebDriver driver;
+	@Given("user opens login portal")
+	public void user_opens_login_portal() {
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\saranya.a.shanmugam\\Downloads\\chromedriver_win32\\chromedriver.exe");
+		  driver=new ChromeDriver();
+		  driver.get("http://newtours.demoaut.com");
+	}
+
+	@When("user enters username as {string}")
+	public void user_enters_username_as(String un) {
+	 // System.out.println("enter username as "+un);
+	  driver.findElement(By.name("userName")).sendKeys(un);
+	}
+
+	@When("user enters password as {string}")
+	public void user_enters_password_as(String psd) {
+	   //System.out.println("enter password as "+psd);
+	  driver.findElement(By.name("password")).sendKeys(psd);
+	}
+
+	@When("Clicks on singn button")
+	public void clicks_on_singn_button() {
+	  // System.out.println("click signin");
+		 driver.findElement(By.xpath("//input[@value='Login']")).click();
+	}
+
+	@Then("validate login success")
+	public void validate_login_success() {
+	    //System.out.println("login sucess");
+		Assert.assertTrue(driver.getTitle().contains("Flight"));
+		  driver.close();
+	}
+
+}
